311686e415b64c8dafca1a2c7814769735cad1a6
Virgile <virgilevallvillellas@gmail.com>
2023-12-06 10:31:07 +0100

    Creation of class Task in Task.py

---
+++ Task.py
@@ -0,0 +1,17 @@
+
+class Task:
+    def __init__(self, title, description, creator, assignee,
+                 creation_date, theoric_completion_date="", completion_date="",
+                 label="", priority="", status=""):
+        self.title = title
+        self.description = description
+        self.creator = creator
+        self.assignee = assignee
+        self.creation_date = creation_date
+        self.theoric_completion_date = theoric_completion_date
+        self.completion_date = completion_date
+        self.label = label
+        self.priority = priority
+        self.status = status
+
+    
