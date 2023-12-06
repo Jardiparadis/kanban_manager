e0569f6f6ffc42816d66332f147add230c3e654e
a@a.a <a@a.a>
2023-12-06 11:15:32 +0100

    display custom colors for columns

--- Kanban.py
+++ Kanban.py
@@ -1,4 +1,5 @@
 import pygame
+from Column import Column
 
 COLUMN_WIDTH = 200
 COLUMN_HEADER_HEIGHT = 50
@@ -8,22 +9,25 @@ COLUMN_SPACES = 40
 
 class Kanban:
     def __init__(self):
-        self.default_columns = ["Open", "Develop", "Close"]
+        self.default_columns = [
+            Column("Open", [], pygame.Color(166, 237, 166), pygame.Color(217, 255, 211)),
+            Column("Develop", [], pygame.Color(237, 193, 166), pygame.Color(255, 233, 211)),
+            Column("Close", [], pygame.Color(237, 166, 166), pygame.Color(255, 211, 211))
+        ]
         self.screen = None
         self.column_left_start_pos = 20
         self.column_top_start_pos = 20
-        pass
 
     def render_columns(self):
         left_pos = self.column_left_start_pos
         top_pos = self.column_top_start_pos
         for column in self.default_columns:
-            pygame.draw.rect(self.screen, pygame.Color(224, 224, 224),
+            pygame.draw.rect(self.screen, column.header_color,
                              pygame.Rect(left_pos, top_pos, COLUMN_WIDTH, COLUMN_HEADER_HEIGHT))
-            pygame.draw.rect(self.screen, pygame.Color(204, 204, 204),
+            pygame.draw.rect(self.screen, column.body_color,
                              pygame.Rect(left_pos, top_pos + COLUMN_HEADER_HEIGHT, COLUMN_WIDTH, COLUMN_BODY_HEIGHT))
             font = pygame.font.SysFont(None, 24)
-            img = font.render(column, True, pygame.Color(39, 39, 39))
+            img = font.render(column.title, True, pygame.Color(39, 39, 39))
             self.screen.blit(img, (left_pos + 10, top_pos + 15))
             left_pos += COLUMN_WIDTH + COLUMN_SPACES
 
